f5e1fe720d6ab66cfdd3f99acbb629267d384f33
HTFDT <almetov_tag@mail.ru>
2024-04-07 21:01:47 +0500

    add kit entity configuration

---
+++ backend/moveMarket/infrastructure/EntityConfigurations/KitConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntityConfigurations;
+
+internal sealed class KitConfiguration : IEntityTypeConfiguration<Kit>
+{
+    public void Configure(EntityTypeBuilder<Kit> builder)
+    {
+        builder.ToTable("Kit");
+    }
+}
