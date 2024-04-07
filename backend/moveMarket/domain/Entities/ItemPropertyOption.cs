65aa3e7a1852a5ee143bcd1b083d82f0330de678
HTFDT <almetov_tag@mail.ru>
2024-04-07 16:36:16 +0500

    add item property option entity

---
+++ backend/moveMarket/domain/Entities/ItemPropertyOption.cs
@@ -0,0 +1,16 @@
+﻿using core.Entities.Base;
+
+namespace domain.Entities;
+
+public class ItemPropertyOption : IEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public string Value { get; set; } = null!;
+    public double PriceMultiplier { get; set; } = 1;
+    public bool IsAvailable { get; set; }
+    public bool IsDefault { get; set; }
+    public string? RelatedImagePath { get; set; }
+
+    public Guid ItemPropertyId { get; set; }
+    public ItemProperty ItemProperty { get; set; } = null!;
+}
