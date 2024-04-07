454fb14988504f0ef7db5c79422e87197a460940
HTFDT <almetov_tag@mail.ru>
2024-04-07 16:37:21 +0500

    add category entity

---
+++ backend/moveMarket/domain/Entities/Category.cs
@@ -0,0 +1,9 @@
+﻿using core.Entities.Base;
+
+namespace domain.Entities;
+
+public class Category : IEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+}
