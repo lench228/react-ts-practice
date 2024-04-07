37837e0d1b824e76eeae149c53941cdb5bec0dc7
HTFDT <almetov_tag@mail.ru>
2024-04-07 20:59:42 +0500

    add property entity configuration

---
+++ backend/moveMarket/infrastructure/EntityConfigurations/PropertyConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntityConfigurations;
+
+internal sealed class PropertyConfiguration : IEntityTypeConfiguration<Property>
+{
+    public void Configure(EntityTypeBuilder<Property> builder)
+    {
+        builder.ToTable("Property");
+    }
+}
