02b4340d1d436e173df5e1f7ec67eae04152e7a5
HTFDT <almetov_tag@mail.ru>
2024-04-07 17:36:25 +0500

    add favorite entity

---
+++ backend/moveMarket/domain/Entities/Favorite.cs
@@ -0,0 +1,13 @@
+﻿using core.Entities.Base;
+
+namespace domain.Entities;
+
+public class Favorite : IEntity<Guid>
+{
+    public Guid Id { get; set; }
+    
+    public Guid UserId { get; set; }
+    public Guid KitId { get; set; }
+    public ApplicationUser User { get; set; } = null!;
+    public Kit Kit { get; set; } = null!;
+}
