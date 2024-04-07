68371d288080766baa7dfd63bbcf9edeb1df8855
HTFDT <almetov_tag@mail.ru>
2024-04-07 20:47:11 +0500

    add cart kit entity configuration

---
+++ backend/moveMarket/infrastructure/EntityConfigurations/CartKitConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntityConfigurations;
+
+internal sealed class CartKitConfiguration : IEntityTypeConfiguration<CartKit>
+{
+    public void Configure(EntityTypeBuilder<CartKit> builder)
+    {
+        builder.HasIndex(ki => new { ki.KitId, ki.CartId })
+            .IsUnique();
+
+        builder.ToTable("CartKit");
+    }
+}
