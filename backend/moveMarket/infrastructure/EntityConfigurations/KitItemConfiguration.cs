a255ad738cfd750bd37dd7caa2c90b2b886ac768
HTFDT <almetov_tag@mail.ru>
2024-04-07 20:40:04 +0500

    add kit item entity configuration

---
+++ backend/moveMarket/infrastructure/EntityConfigurations/KitItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntityConfigurations;
+
+internal sealed class KitItemConfiguration : IEntityTypeConfiguration<KitItem>
+{
+    public void Configure(EntityTypeBuilder<KitItem> builder)
+    {
+        builder.HasIndex(ki => new { ki.KitId, ki.ItemId })
+            .IsUnique();
+
+        builder.ToTable("KitItem");
+    }
+}
