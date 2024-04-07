ae7253357168a5be40e061eb068b4e9252fd0579
HTFDT <almetov_tag@mail.ru>
2024-04-07 20:41:19 +0500

    add item property entity configuration

---
+++ backend/moveMarket/infrastructure/EntityConfigurations/ItemPropertyConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntityConfigurations;
+
+internal sealed class ItemPropertyConfiguration : IEntityTypeConfiguration<ItemProperty>
+{
+    public void Configure(EntityTypeBuilder<ItemProperty> builder)
+    {
+        builder.HasIndex(ki => new { ki.PropertyId, ki.ItemId })
+            .IsUnique();
+
+        builder.ToTable("ItemProperty");
+    }
+}
