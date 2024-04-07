1138a7da1e63affcfbdc1dc084555189d1aaf478
HTFDT <almetov_tag@mail.ru>
2024-04-07 16:10:56 +0500

    add application user entity

---
+++ backend/moveMarket/domain/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using core.Entities.Base;
+using Microsoft.AspNetCore.Identity;
+
+namespace domain.Entities;
+
+public class ApplicationUser : IdentityUser<Guid>, IEntity<Guid>
+{
+    public string? AvatarPath { get; set; }
+    public string? RefreshToken { get; set; }
+}
