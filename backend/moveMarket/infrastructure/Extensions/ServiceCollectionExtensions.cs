1dccc927a6a4b508ee55431b1d587de2e071bcca
HTFDT <almetov_tag@mail.ru>
2024-04-07 21:44:34 +0500

    add infrastructure service collection extensions

---
+++ backend/moveMarket/infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(config.GetConnectionString("DefaultConnection")));
+        services.AddIdentityCore<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.User.RequireUniqueEmail = true;
+                })
+            .AddRoles<IdentityRole<Guid>>()
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+        return services;
+    }
+}
