1688f27b6e6dc7c10893b10c11e706705168d3a2
HTFDT <almetov_tag@mail.ru>
2024-04-07 21:00:45 +0500

    add category entity configuration

---
+++ backend/moveMarket/infrastructure/EntityConfigurations/CategoryConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntityConfigurations;
+
+internal sealed class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Category");
+    }
+}
