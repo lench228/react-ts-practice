efcc478e6ab24ab0f97e15054906584f500a75c4
HTFDT <almetov_tag@mail.ru>
2024-04-07 20:54:48 +0500

    add item property option entity configuration

---
+++ backend/moveMarket/infrastructure/EntityConfigurations/ItemPropertyOptionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntityConfigurations;
+
+internal sealed class ItemPropertyOptionConfiguration : IEntityTypeConfiguration<ItemPropertyOption>
+{
+    public void Configure(EntityTypeBuilder<ItemPropertyOption> builder)
+    {
+        builder.HasIndex(e => e.IsDefault)
+            .HasFilter("\"IsDefault\" = true")
+            .IsUnique();
+
+        builder.ToTable("ItemPropertyOption");
+    }
+}
