09d5513de43b016525748bf2025e5e62f9dde8e4
HTFDT <almetov_tag@mail.ru>
2024-04-07 20:45:35 +0500

    add cart kit item selected property option entity configuration

---
+++ backend/moveMarket/infrastructure/EntityConfigurations/CartKitItemSelectedPropertyOptionConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntityConfigurations;
+
+internal sealed class CartKitItemSelectedPropertyOptionConfiguration :
+    IEntityTypeConfiguration<CartKitItemSelectedPropertyOption>
+{
+    public void Configure(EntityTypeBuilder<CartKitItemSelectedPropertyOption> builder)
+    {
+        builder.HasIndex(e => new { e.CartKitItemId, e.ItemPropertyId })
+            .IsUnique();
+
+        builder.ToTable("CartKitItemSelectedPropertyOption");
+    }
+}
