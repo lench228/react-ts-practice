a657c0acfe38bdace5f62b2d3c90210d70662867
HTFDT <almetov_tag@mail.ru>
2024-04-07 21:02:33 +0500

    add item entity configuration

---
+++ backend/moveMarket/infrastructure/EntityConfigurations/ItemConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntityConfigurations;
+
+internal sealed class ItemConfiguration : IEntityTypeConfiguration<Item>
+{
+    public void Configure(EntityTypeBuilder<Item> builder)
+    {
+        builder.ToTable("Item");
+    }
+}
