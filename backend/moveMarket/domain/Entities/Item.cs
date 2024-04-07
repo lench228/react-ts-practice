a06f8f4bce27111b7c8c2d5844bfcdc7c950d902
HTFDT <almetov_tag@mail.ru>
2024-04-07 16:26:15 +0500

    add item entity

---
+++ backend/moveMarket/domain/Entities/Item.cs
@@ -0,0 +1,12 @@
+﻿using core.Entities.Base;
+
+namespace domain.Entities;
+
+public class Item : IEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string? Description { get; set; }
+    public double Price { get; set; }
+    public string? ImagePath { get; set; }
+}
