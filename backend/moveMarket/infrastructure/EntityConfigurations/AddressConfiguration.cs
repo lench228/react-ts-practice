4d95b6c4c8cf7d496ac267f61ee212f3c05d9ee2
HTFDT <almetov_tag@mail.ru>
2024-04-07 21:00:21 +0500

    add address entity configuration

---
+++ backend/moveMarket/infrastructure/EntityConfigurations/AddressConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace infrastructure.EntityConfigurations;
+
+internal sealed class AddressConfiguration : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.ToTable("Address");
+    }
+}
