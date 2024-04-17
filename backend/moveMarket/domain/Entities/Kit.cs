ce5462dfef991c80c53938eb071855b04f261984
HTFDT <almetov_tag@mail.ru>
2024-04-17 20:50:21 +0500

    add kit entity normalized name

--- backend/moveMarket/domain/Entities/Kit.cs
+++ backend/moveMarket/domain/Entities/Kit.cs
@@ -6,6 +6,7 @@ public class Kit : IEntity<Guid>
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
+    public string? NormalizedName { get; set; } = null!;
     public string Description { get; set; } = null!;
     public double Discount { get; set; }
     public long Popularity { get; set; }
