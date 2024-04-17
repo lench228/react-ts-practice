8d04d615eaf224fb3dfe72a255092aa776814888
HTFDT <almetov_tag@mail.ru>
2024-04-17 20:51:11 +0500

    add property entity normalized name

--- backend/moveMarket/domain/Entities/Property.cs
+++ backend/moveMarket/domain/Entities/Property.cs
@@ -6,6 +6,7 @@ public class Property : IEntity<Guid>
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = null!;
+    public string? NormalizedName { get; set; } = null!;
     
     public ICollection<ItemProperty> ItemProperties { get; set; } = null!;
 }
