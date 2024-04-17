041b5b1f777725be2d179b9ff0e26c8bd28d82e1
HTFDT <almetov_tag@mail.ru>
2024-04-17 20:51:47 +0500

    set unique constraint to category entity normalized name

--- backend/moveMarket/infrastructure/EntityConfigurations/CategoryConfiguration.cs
+++ backend/moveMarket/infrastructure/EntityConfigurations/CategoryConfiguration.cs
@@ -9,5 +9,11 @@ internal sealed class CategoryConfiguration : IEntityTypeConfiguration<Category>
     public void Configure(EntityTypeBuilder<Category> builder)
     {
         builder.ToTable("Category");
+        
+        builder.HasIndex(e => e.NormalizedName)
+            .IsUnique();
+
+        builder.Property(e => e.NormalizedName)
+            .HasComputedColumnSql("UPPER(\"Name\")", true);
     }
 }
