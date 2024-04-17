1b9d81e76b8d92c8a8028cec0ee40d2212724bd3
HTFDT <almetov_tag@mail.ru>
2024-04-17 20:54:24 +0500

    add database updater static class

---
+++ backend/moveMarket/infrastructure/DatabaseUpdater.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace infrastructure;
+
+public static class DatabaseUpdater
+{
+    public static void UpdateDatabase(IServiceProvider serviceProvider)
+    {
+        var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+}
