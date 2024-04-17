3ca12cad46a861ab01bc90ec253db5f480c08587
HTFDT <almetov_tag@mail.ru>
2024-04-17 20:54:37 +0500

    add seed data static class

---
+++ backend/moveMarket/infrastructure/SeedData.cs
@@ -0,0 +1,28 @@
+﻿using domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace infrastructure;
+
+public static class SeedData
+{
+    public static async Task EnsureUsersCreated(IEnumerable<(string email, string password)> users,
+        IServiceProvider serviceProvider)
+    {
+        var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+        foreach (var user in users)
+        {
+            var exists = await userManager.Users
+                .SingleOrDefaultAsync(u => u.Email == user.email);
+            if (exists is not null)
+                continue;
+            var newUser = new ApplicationUser
+            {
+                Email = user.email,
+                UserName = user.email
+            };
+            await userManager.CreateAsync(newUser, user.password);
+        }
+    }
+}
