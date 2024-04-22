4984f19897d2179cc87d8a6c637cee643e0f245e
HTFDT <almetov_tag@mail.ru>
2024-04-22 20:22:30 +0500

    add generic repository interface

---
+++ backend/moveMarket/domain/Interfaces/IRepository.cs
@@ -0,0 +1,9 @@
+﻿using core.Entities.Base;
+
+namespace domain.Interfaces;
+
+public interface IRepository<TEntity> : ITypedRepository<Guid, TEntity>
+    where TEntity: IEntity<Guid>
+{
+    
+}
---
+++ backend/moveMarket/domain/Interfaces/ITypedRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using core.Entities.Base;
+
+namespace domain.Interfaces;
+
+public interface ITypedRepository<TKey, TEntity> 
+    where TEntity : IEntity<TKey>
+{
+    Task<bool> CheckExistsByIdAsync(TKey id);
+    Task CreateAsync(TEntity entity);
+    Task CreateManyAsync(IEnumerable<TEntity> entities);
+    Task<TEntity?> GetByIdAsync(TKey id);
+    Task UpdateAsync(TEntity entity);
+    Task DeleteAsync(TEntity entity);
+    IQueryable<TEntity> Query();
+    Task LoadReference<TProperty>(TEntity entity, Expression<Func<TEntity, TProperty?>> property)
+        where TProperty : class;
+    Task LoadCollection<TProperty>(TEntity entity, Expression<Func<TEntity, IEnumerable<TProperty>>> property)
+        where TProperty : class;
+}
