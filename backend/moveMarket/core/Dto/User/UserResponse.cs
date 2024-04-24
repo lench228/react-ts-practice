6dfebe07d7e82ef83f830f880d15e332ac639207
HTFDT <almetov_tag@mail.ru>
2024-04-24 16:34:10 +0500

    add user response dto

---
+++ backend/moveMarket/core/Dto/User/UserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace core.Dto.User;
+
+public record UserResponse(Guid UserId, string Login, string? UserName)
+{
+    public UserResponse() : this(Guid.Empty, string.Empty, string.Empty)
+    {
+    }
+}
