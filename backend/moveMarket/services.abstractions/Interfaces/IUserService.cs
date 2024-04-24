5423cf457a001f40b431bc83512aad642808495b
HTFDT <almetov_tag@mail.ru>
2024-04-24 16:38:53 +0500

    add IUserService interface

---
+++ backend/moveMarket/services.abstractions/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using core.Dto.Auth;
+using core.Dto.User;
+
+namespace services.abstractions.Interfaces;
+
+public interface IUserService
+{
+    Task<UserResponse> GetByEmailAsync(string email);
+    Task<UserResponse> GetByIdAsync(Guid id);
+    Task<CreateUserResponse> CreateUserAsync(CreateUserRequest request);
+    Task<UserAddressResponse> UpdateUserAsync(ClaimsPrincipal userPrincipal, UpdateUserRequest request);
+    Task ChangePasswordAsync(ClaimsPrincipal userPrincipal, ChangePasswordRequest request);
+    Task<UserAddressResponse> SetAddressAsync(ClaimsPrincipal userPrincipal, SetAddressRequest request);
+    Task<LoginResponse> LoginAsync(LoginRequest request);
+    Task RevokeTokenAsync(ClaimsPrincipal userPrincipal);
+    Task<RefreshTokenResponse> RefreshTokenAsync(RefreshTokenRequest request);
+}
