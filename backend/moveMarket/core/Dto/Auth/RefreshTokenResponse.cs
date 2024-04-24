b5fb73b5260568e2e22dbdca6239ccbac904075c
HTFDT <almetov_tag@mail.ru>
2024-04-24 16:36:55 +0500

    add refresh token response dto

---
+++ backend/moveMarket/core/Dto/Auth/RefreshTokenResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace core.Dto.Auth;
+
+public record RefreshTokenResponse(Guid UserId,
+    string Login,
+    string? UserName,
+    string AccessToken, 
+    string RefreshToken,
+    long AccessTokenLifetimeSeconds, 
+    long RefreshTokenLifetimeSeconds);
