644e5ff5803755126ee7eecd61550959287a6061
HTFDT <almetov_tag@mail.ru>
2024-04-24 16:35:43 +0500

    add user with address response dto

---
+++ backend/moveMarket/core/Dto/User/UserAddressResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace core.Dto.User;
+
+public record UserAddressResponse(Guid UserId, string Login, string? UserName, AddressResponse? Address)
+{
+    public UserAddressResponse() : this(Guid.Empty, string.Empty, string.Empty, null)
+    {
+    }
+}
