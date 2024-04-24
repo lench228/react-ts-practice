f997cb00145106a1db53723d13bfe516799992c1
HTFDT <almetov_tag@mail.ru>
2024-04-24 16:32:57 +0500

    add create user response dto

---
+++ backend/moveMarket/core/Dto/Auth/CreateUserResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace core.Dto.Auth;
+
+public record CreateUserResponse(Guid UserId,
+   string Login,
+    string? UserName,  
+    string AccessToken,
+    string RefreshToken,
+    long AccessTokenLifetimeSeconds, 
+    long RefreshTokenLifetimeSeconds);
