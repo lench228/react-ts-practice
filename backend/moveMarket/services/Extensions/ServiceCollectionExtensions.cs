2c74fc6b7bafe9daef08275f85ca380339e85669
HTFDT <almetov_tag@mail.ru>
2024-04-25 18:59:08 +0500

    add services service collection extensions

---
+++ backend/moveMarket/services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using services.abstractions.Interfaces;
+using services.Implementations;
+
+namespace services.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUserService, UserService>();
+        return services;
+    }
+}
