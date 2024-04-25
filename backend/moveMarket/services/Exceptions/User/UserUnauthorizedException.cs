a79879ccf0f00f8adbec1251691806a524e0d5d6
HTFDT <almetov_tag@mail.ru>
2024-04-25 18:51:51 +0500

    add UserUnauthorizedException

---
+++ backend/moveMarket/services/Exceptions/User/UserUnauthorizedException.cs
@@ -0,0 +1,17 @@
+﻿using core.Dto.Auth;
+using core.Exceptions.Base;
+
+namespace services.Exceptions.User;
+
+public class UserUnauthorizedException : UnauthorizedException
+{
+    public UserUnauthorizedException(LoginRequest request) : base("Wrong password")
+    {
+        ExceptionObject = request;
+    }    
+    
+    public UserUnauthorizedException(RefreshTokenRequest request) : base("Wrong access or refresh token")
+    {
+        ExceptionObject = request;
+    }    
+}
