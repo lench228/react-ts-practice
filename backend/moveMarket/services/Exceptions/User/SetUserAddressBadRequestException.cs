bd0f8ddf6ac71cf2d4820e2929b282bbfc5d6ec3
HTFDT <almetov_tag@mail.ru>
2024-04-25 18:51:23 +0500

    add SetUserAddressBadRequestException

---
+++ backend/moveMarket/services/Exceptions/User/SetUserAddressBadRequestException.cs
@@ -0,0 +1,12 @@
+﻿using core.Dto.User;
+using core.Exceptions.Base;
+
+namespace services.Exceptions.User;
+
+public class SetUserAddressBadRequestException : BadRequestException
+{
+    public SetUserAddressBadRequestException(string message, SetAddressRequest request) : base(message)
+    {
+        ExceptionObject = request;
+    }
+}
