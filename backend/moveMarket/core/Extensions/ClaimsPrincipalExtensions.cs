50fa4df9fa2963b87e717bc9fbb5711f1fd31995
HTFDT <almetov_tag@mail.ru>
2024-04-25 18:54:38 +0500

    add ClaimsPrincipalExtensions

---
+++ backend/moveMarket/core/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Claims;
+
+namespace core.Extensions;
+
+public static class ClaimsPrincipalExtensions
+{
+    public static bool TryGetUserId(this ClaimsPrincipal principal, out Guid userId)
+    {
+        userId = Guid.Empty;
+        var userIdClaim = principal.FindFirst(ClaimTypes.NameIdentifier);
+        return userIdClaim is not null && Guid.TryParse(userIdClaim.Value, out userId);
+    }
+}
