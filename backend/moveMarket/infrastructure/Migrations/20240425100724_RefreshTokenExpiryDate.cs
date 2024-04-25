ca1aa0a353e9076be6b831c8e785b2f019b306ff
HTFDT <almetov_tag@mail.ru>
2024-04-25 15:09:54 +0500

    add user refresh token expiry date migration

---
+++ backend/moveMarket/infrastructure/Migrations/20240425100724_RefreshTokenExpiryDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshTokenExpiryDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefreshTokenExpiryDate",
+                table: "User",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefreshTokenExpiryDate",
+                table: "User");
+        }
+    }
+}
