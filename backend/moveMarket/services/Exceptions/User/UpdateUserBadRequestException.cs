6b6687d2dd145fa917984e2a2a433c7635933bde
HTFDT <almetov_tag@mail.ru>
2024-04-25 18:51:32 +0500

    add UpdateUserBadRequestException

---
+++ backend/moveMarket/services/Exceptions/User/UpdateUserBadRequestException.cs
@@ -0,0 +1,11 @@
+﻿using core.Exceptions.Base;
+
+namespace services.Exceptions.User;
+
+public class UpdateUserBadRequestException : BadRequestException
+{
+    public UpdateUserBadRequestException(string message, object exceptionObject) : base(message)
+    {
+        ExceptionObject = exceptionObject;
+    }
+}
