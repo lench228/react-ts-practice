d51ad02f6fce3d1cb77b7cb75018aa96f7d3883a
HTFDT <almetov_tag@mail.ru>
2024-04-25 18:51:43 +0500

    add UserNotFoundException

---
+++ backend/moveMarket/services/Exceptions/User/UserNotFoundException.cs
@@ -0,0 +1,20 @@
+﻿using core.Dto.User;
+using core.Exceptions.Base;
+using domain.Entities;
+
+namespace services.Exceptions.User;
+
+public class UserNotFoundException : NotFoundException
+{
+    public UserNotFoundException(string email) 
+        : base("User with this login is not found")
+    {
+        ExceptionObject = email;
+    }
+
+    public UserNotFoundException(Guid id) 
+        : base("User with this id is not found")
+    {
+        ExceptionObject = id;
+    }
+}
