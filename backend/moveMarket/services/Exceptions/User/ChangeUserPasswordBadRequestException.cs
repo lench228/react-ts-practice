3ab0c5475f39f2f4126ace2b51e9f8af1ba101f7
HTFDT <almetov_tag@mail.ru>
2024-04-25 18:50:25 +0500

    add ChangeUserPasswordBadRequestException

---
+++ backend/moveMarket/services/Exceptions/User/ChangeUserPasswordBadRequestException.cs
@@ -0,0 +1,13 @@
+﻿using core.Dto.User;
+using core.Exceptions.Base;
+
+namespace services.Exceptions.User;
+
+public class ChangeUserPasswordBadRequestException : BadRequestException
+{
+    public ChangeUserPasswordBadRequestException(string message, ChangePasswordRequest exceptionObject) 
+        : base(message)
+    {
+        ExceptionObject = exceptionObject;
+    }
+}
