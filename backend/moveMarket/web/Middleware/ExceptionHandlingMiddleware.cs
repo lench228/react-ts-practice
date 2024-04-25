594ac3b9186b06f2db102f27511cbdf409e155d5
HTFDT <almetov_tag@mail.ru>
2024-04-25 19:00:15 +0500

    add ExceptionHandlingMiddleware

---
+++ backend/moveMarket/web/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json;
+using core.Exceptions.Base;
+
+namespace web.Middleware;
+
+public class ExceptionHandlingMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (BaseResponseException e)
+        {
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, BaseResponseException exception)
+    {
+        context.Response.StatusCode = exception switch
+        {
+            BadRequestException => StatusCodes.Status400BadRequest,
+            NotFoundException => StatusCodes.Status404NotFound,
+            ConflictException => StatusCodes.Status409Conflict,
+            ForbiddenException => StatusCodes.Status403Forbidden,
+            UnauthorizedException => StatusCodes.Status401Unauthorized,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        var response = new { exception.Message, exception.ExceptionObject };
+
+        await context.Response.WriteAsJsonAsync(response);
+    }
+}
