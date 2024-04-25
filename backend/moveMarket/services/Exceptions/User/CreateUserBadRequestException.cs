636b53ffc793ff80d43e12304fb59176550dead7
HTFDT <almetov_tag@mail.ru>
2024-04-25 18:50:15 +0500

    add CreateUserBadRequestException

---
+++ backend/moveMarket/services/Exceptions/User/CreateUserBadRequestException.cs
@@ -0,0 +1,12 @@
+﻿using core.Dto.Auth;
+using core.Exceptions.Base;
+
+namespace services.Exceptions.User;
+
+public class CreateUserBadRequestException : BadRequestException
+{
+    public CreateUserBadRequestException(string message, CreateUserRequest request) : base(message)
+    {
+        ExceptionObject = request;
+    }
+}
