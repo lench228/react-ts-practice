b7dbb4ded6a5d755b14081ed732422132e3473eb
fulprimitiv <eremeevdima51@gmail.com>
2024-05-30 06:19:54 +0500

    completed my favorites

--- frontend/src/favorites.js
+++ frontend/src/favorites.js
@@ -1,56 +1,65 @@
-const text = document.getElementById("text");
-const text1 = document.getElementById("text1");
-const text2 = document.getElementById("text2");
-const text3 = document.getElementById("text3");
-const logoIcon = document.getElementById("logoIcon");
-const favouriteIcon = document.getElementById("favouriteIcon");
-const bagIcon = document.getElementById("bagIcon");
-const profileIcon = document.getElementById("profileIcon");
+import { USERS, PRODUCTS } from './mock.js';
 
-if (text) {
-	text.addEventListener("click", function (e) {
-		window.location.href = "./profile.html";
-	});
+document.addEventListener('DOMContentLoaded', function () {
+  let currentUser = USERS[0];
+  const savedFavorites = localStorage.getItem('userFavorites');
+
+  if (savedFavorites) {
+    currentUser.favorites = JSON.parse(savedFavorites);
+  }
+
+  PRODUCTS.forEach(product => {
+    product.isFavorite = currentUser.favorites.includes(product.id);
+  });
+
+  const favouriteList = document.getElementById('favourite-list');
+  const favoriteProducts = PRODUCTS.filter(product => product.isFavorite);
+  favouriteList.innerHTML = favoriteProducts.map(createProductItem).join('');
+  const heartButtons = document.querySelectorAll('.heart-button');
+
+  heartButtons.forEach(button => {
+    button.addEventListener('click', function () {
+      const heartIcon = this.querySelector('.heart-icon');
+      const productId = this.closest('.item').dataset.productId;
+      const product = PRODUCTS.find(p => p.id === productId);
+      product.isFavorite = !product.isFavorite;
+
+      if (product.isFavorite) {
+        currentUser.favorites.push(product.id);
+        heartIcon.src = '/frontend/public/img/svg/fill-heart.svg';
+      } else {
+        currentUser.favorites = currentUser.favorites.filter(favId => favId !== product.id);
+        heartIcon.src = '/frontend/public/img/svg/empty-heart.svg';
+      }
+
+      localStorage.setItem('userFavorites', JSON.stringify(currentUser.favorites));
+      localStorage.setItem('products', JSON.stringify(PRODUCTS));
+    });
+  });
+});
+
+function createProductItem(product) {
+  const discountedPrice = product.tag === 'sale' ? Math.floor(product.price * 0.8) : null;
+  const originalPriceStyle = (product.tag === '' || product.tag === 'new') ? 'color: var(--color-black);' : 'text-decoration: line-through; color: var(--color1);';
+
+  return `
+    <div class="item" data-product-id="${product.id}">
+      <div class="image-wrapper">
+        <img class="item-img" src="${product.img}" alt="${product.name}" />
+        ${product.tag ? `<div class="tag ${product.tag === 'new' ? 'new' : 'discount'}">${product.tag}</div>` : ''}
+        <button class="heart-button">
+          <img src="${product.isFavorite ? '/frontend/public/img/svg/fill-heart.svg' : '/frontend/public/img/svg/empty-heart.svg'}" alt="Heart Icon" class="heart-icon">
+        </button>
+      </div>
+      <div class="item-details">
+        <div class="price">
+          <span class="discounted-price" style="${originalPriceStyle}">${product.price} ₽</span>
+          ${discountedPrice ? `<span class="discounted-price">${discountedPrice} ₽</span>` : ''}
+        </div>
+        <div class="item-name">${product.name}</div>
+      </div>
+    </div>
+  `;
 }
 
-if (text1) {
-	text1.addEventListener("click", function (e) {
-		window.location.href = "./profile.html";
-	});
-}
-
-if (text2) {
-	text2.addEventListener("click", function (e) {
-		window.location.href = "";
-	});
-}
-
-if (text3) {
-	text3.addEventListener("click", function (e) {
-		window.location.href = "index.html";
-	});
-}
-
-if (logoIcon) {
-	logoIcon.addEventListener("click", function (e) {
-		window.location.href = "index.html";
-	});
-}
-
-if (favouriteIcon) {
-	favouriteIcon.addEventListener("click", function (e) {
-		window.location.href = "";
-	});
-}
-
-if (bagIcon) {
-	bagIcon.addEventListener("click", function (e) {
-		window.location.href = "";
-	});
-}
-
-if (profileIcon) {
-	profileIcon.addEventListener("click", function (e) {
-		window.location.href = "./profile.html";
-	});
-}
+document.getElementById('favourite-list').innerHTML = PRODUCTS.map(createProductItem).join('');
