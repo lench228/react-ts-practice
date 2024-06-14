71ae021227ad7b05a338036ad7950ae0cb5bf96c
HTFDT <almetov_tag@mail.ru>
2024-06-14 09:21:18 +0500

    add AddToFavoriteBadRequestException

---
+++ backend/moveMarket/services/Exceptions/Kits/AddToFavoriteBadRequestException.cs
@@ -0,0 +1,12 @@
+﻿using core.Exceptions.Base;
+using Microsoft.AspNetCore.Identity;
+
+namespace services.Exceptions.Kits;
+
+public class AddToFavoriteBadRequestException : BadRequestException
+{
+    public AddToFavoriteBadRequestException(string message) : base("can't add the kit to the users' favorites")
+    {
+        ExceptionObject = message;
+    }
+}
