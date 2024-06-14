83c3564ea2e8ef5c6aa5b58aa7ab0ce1cc51e659
HTFDT <almetov_tag@mail.ru>
2024-06-14 09:21:06 +0500

    add KitNotFoundException

---
+++ backend/moveMarket/services/Exceptions/Kits/KitNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using core.Exceptions.Base;
+
+namespace services.Exceptions.Kits;
+
+public class KitNotFoundException : NotFoundException
+{
+    public KitNotFoundException(Guid kitId) : base("Kit with this id is not found")
+    {
+        ExceptionObject = kitId;
+    }
+}
