787680f0e3aa80c17e637092719e1f8aef3289fb
HTFDT <almetov_tag@mail.ru>
2024-06-15 11:12:40 +0500

    add UnitOfWork implementation

---
+++ backend/moveMarket/infrastructure/Implementations/UnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using domain.Interfaces;
+
+namespace infrastructure.Implementations;
+
+internal class UnitOfWork(ApplicationDbContext context) : IUnitOfWork
+{
+    public Task SaveChangesAsync()
+    {
+        return context.SaveChangesAsync();
+    }
+}
