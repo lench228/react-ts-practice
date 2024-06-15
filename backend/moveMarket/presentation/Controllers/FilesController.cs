b0b02a66abfdc5609c779f180039e424bc15c38f
HTFDT <almetov_tag@mail.ru>
2024-06-15 20:23:35 +0500

    add FilesController

---
+++ backend/moveMarket/presentation/Controllers/FilesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace presentation.Controllers;
+
+public class FilesController(IWebHostEnvironment env, IConfiguration config) : Controller
+{
+    [HttpGet("api/files/{imageId}")]
+    public IActionResult GetImage(string imageId)
+    {
+        var path = Path.Combine(env.ContentRootPath, config["ImagesDirPath"]!);
+        var imagePath = Directory.GetFiles(path, $"{imageId}.*").FirstOrDefault();
+        if (imagePath == null)
+            return NotFound();
+        var stream = new FileStream(imagePath, FileMode.Open);
+        return File(stream, $"image/{Path.GetExtension(imagePath).TrimStart('.')}");
+    }
+}
