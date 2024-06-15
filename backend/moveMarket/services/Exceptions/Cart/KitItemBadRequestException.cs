dfcfd1157401798e323b05c28b06f6e978bec8cc
HTFDT <almetov_tag@mail.ru>
2024-06-15 11:13:32 +0500

    add KitItemBadRequestException

---
+++ backend/moveMarket/services/Exceptions/Cart/KitItemBadRequestException.cs
@@ -0,0 +1,11 @@
+﻿using core.Exceptions.Base;
+
+namespace services.Exceptions.Cart;
+
+public class KitItemBadRequestException : BadRequestException
+{
+    public KitItemBadRequestException(object excObject) : base("no such item in this kit")
+    {
+        ExceptionObject = excObject;
+    }
+}
