6385c70ea958834c01068b8532e3bc08dadde85f
HTFDT <almetov_tag@mail.ru>
2024-06-15 11:05:19 +0500

    add OptionResponse dto

---
+++ backend/moveMarket/core/Dto/Properties/OptionResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace core.Dto.Properties;
+
+public record OptionResponse(Guid OptionId,
+    string Value,
+    string? ImageId,
+    double PriceMultiplier,
+    bool IsAvailable,
+    bool IsDefault);
