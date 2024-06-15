bee5d412939b041e62491c6d4569a18fd09e8793
HTFDT <almetov_tag@mail.ru>
2024-06-15 11:18:37 +0500

    add IKitsService interface

---
+++ backend/moveMarket/services.abstractions/Interfaces/IKitsService.cs
@@ -0,0 +1,9 @@
+﻿using core.Dto.Kits;
+
+namespace services.abstractions.Interfaces;
+
+public interface IKitsService
+{
+    Task<KitResponse> GetKitByIdAsync(Guid kitId);
+    Task<IEnumerable<KitItemResponse>> GetKitItemsAsync(Guid kitId);
+}
