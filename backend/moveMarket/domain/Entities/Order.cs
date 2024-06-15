180e9ec6a14e188982f81865e0adf8fd548e090f
HTFDT <almetov_tag@mail.ru>
2024-06-15 15:42:08 +0500

    add Order entity

---
+++ backend/moveMarket/domain/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿using core.Entities.Base;
+using core.Enums;
+
+namespace domain.Entities;
+
+public class Order : IEntity<Guid>
+{
+    public Guid Id { get; set; }
+    public OrderStatus Status { get; set; }
+    public long Quantity { get; set; }
+    public double Price { get; set; }
+    
+    public Guid UserId { get; set; }
+    public ApplicationUser User { get; set; } = null!;
+    public Guid CartKitId { get; set; }
+    public CartKit CartKit { get; set; } = null!;
+}
