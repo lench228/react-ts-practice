caf07d810d4961a35df056687f15a4054002b059
HTFDT <almetov_tag@mail.ru>
2024-06-15 11:13:57 +0500

    add PropertyNotFoundException

---
+++ backend/moveMarket/services/Exceptions/Properties/PropertyNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using core.Exceptions.Base;
+
+namespace services.Exceptions.Properties;
+
+public class PropertyNotFoundException : NotFoundException
+{
+    public PropertyNotFoundException(Guid id) : base("Property with this id is not found")
+    {
+        ExceptionObject = id;
+    }
+}
