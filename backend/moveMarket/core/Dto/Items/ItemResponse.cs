de6e62bd0a80277bb3a1efc8592cd3c50e986c20
HTFDT <almetov_tag@mail.ru>
2024-06-15 11:49:23 +0500

    add ItemResponse dto

---
+++ backend/moveMarket/core/Dto/Items/ItemResponse.cs
@@ -0,0 +1,10 @@
+﻿using core.Dto.Properties;
+
+namespace core.Dto.Items;
+
+public record ItemResponse(Guid ItemId,
+    string Name,
+    string? Description,
+    string? ImageId,
+    double Price,
+    IEnumerable<ItemPropertyResponse> Properties);
