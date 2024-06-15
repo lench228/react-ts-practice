bbb292145ed9e5ccb097eb1b95147cae31c64f31
HTFDT <almetov_tag@mail.ru>
2024-06-15 11:15:07 +0500

    add CartKitNotFoundException

---
+++ backend/moveMarket/services/Exceptions/Cart/CartKitNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using core.Exceptions.Base;
+
+namespace services.Exceptions.Cart;
+
+public class CartKitNotFoundException : NotFoundException
+{
+    public CartKitNotFoundException(Guid cartKitId) : base("CartKit with this id is not found")
+    {
+        ExceptionObject = cartKitId;
+    }
+}
