a93e3ed7ad33f63e6e236940d7ed956028520f8a
HTFDT <almetov_tag@mail.ru>
2024-06-15 16:02:59 +0500

    add IOrdersService interface

---
+++ backend/moveMarket/services.abstractions/Interfaces/IOrdersService.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using core.Dto.Orders;
+using core.Enums;
+
+namespace services.abstractions.Interfaces;
+
+public interface IOrdersService
+{
+    Task<CreateOrderResponse> CreateOrderAsync(ClaimsPrincipal userPrincipal, CreateOrderRequest request);
+    Task<OrderResponse> GetOrder(ClaimsPrincipal userPrincipal, Guid orderId);
+    Task<IEnumerable<OrderResponse>> GetUserOrders(ClaimsPrincipal userPrincipal, OrderStatus? status);
+}
