043f93cd242b8337eac9d19b2ff7263da721b4ac
HTFDT <almetov_tag@mail.ru>
2024-06-15 18:53:24 +0500

    add OrderActionForbiddenException

---
+++ backend/moveMarket/services/Exceptions/Orders/OrderActionForbiddenException.cs
@@ -0,0 +1,12 @@
+﻿using core.Exceptions.Base;
+
+namespace services.Exceptions.Orders;
+
+public class OrderActionForbiddenException : ForbiddenException
+{
+    public OrderActionForbiddenException(Guid orderId) : 
+        base("The current user is not allowed to perform this action to this order")
+    {
+        ExceptionObject = orderId;
+    }
+}
