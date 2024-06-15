469a18fb9a15662a73f46bc613e2999de27599ba
HTFDT <almetov_tag@mail.ru>
2024-06-15 12:48:57 +0500

    add CategoryNotFoundException

---
+++ backend/moveMarket/services/Exceptions/Categories/CategoryNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using core.Exceptions.Base;
+
+namespace services.Exceptions.Categories;
+
+public class CategoryNotFoundException : NotFoundException
+{
+    public CategoryNotFoundException(Guid categoryId) : base("Category not found")
+    {
+        ExceptionObject = categoryId;
+    }
+}
